21b9bdf954c37e25377ea11a55bf00b493b2d045
gaoqs313 <gaoqs313@gmail.com>
2020-06-16 19:18:58 -0500

    add pipeline

--- run_tophat_neuron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=/scratch_space/qgao/Penultimate/2.data/neuron
-
-cat /scratch_space/qgao/Penultimate/2.data/neuron_SRR_Acc_List.txt | while read sample
-do
-	lsf_prio 4 4 $sample bash tophat_mouse.sh $sample $DIR/$sample\_1.fastq.gz $DIR/$sample\_2.fastq.gz
-done
